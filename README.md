175fb7de6f92b320d4ee1f59e610f46e0a850eba
BNdungu <bryannganga909@gmail.com>
2022-12-09 15:08:49 +0300

    blink

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Pico-C-C-SDK-
+
+# How I created my first blink sketch using th C/C++ SDK for the pico
+
+created a new directory <mkdir project>
+opened the directory    <cd project>
+copied pico_sdk_import.c from the pico-sdk folder <cp ~/pico/pico-sdk/external/pico_sdk_import.cmake .>
+Created a CMakefile <sudo nano CMakeLists.txt> which helps build the code.
+Created the blink.c file <sudo nano blink.c> and in the file edit the code to blink the led.
+Create a build directory <mkdir build>
+Open the build directory <cd build>
+ Export the pico sdk to the build directory so as the compiler knows where to find the sdk <export PICO_SDK_PATH=.../.../pico/pico-sdk> <!--be sure to add your correct path here. -->
+
+<cmake ..> start the build chain using the above stated CMakeLists file
+<make> to have create uf2 file that will be copied to the Raspberry pi Pico
---
+++ project/build/CMakeFiles/blink.dir/home/brian/pico/pico-sdk/src/rp2_common/hardware_clocks/clocks.c.obj.d
@@ -0,0 +1,48 @@
+CMakeFiles/blink.dir/home/brian/pico/pico-sdk/src/rp2_common/hardware_clocks/clocks.c.obj: \
+ /home/brian/pico/pico-sdk/src/rp2_common/hardware_clocks/clocks.c \
+ /home/brian/pico/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/brian/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/brian/pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /home/brian/cuspico/build/generated/pico_base/pico/version.h \
+ /home/brian/pico/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /home/brian/cuspico/build/generated/pico_base/pico/config_autogen.h \
+ /home/brian/pico/pico-sdk/src/boards/include/boards/pico.h \
+ /home/brian/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/brian/pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /home/brian/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/brian/pico/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/clocks.h \
+ /home/brian/pico/pico-sdk/src/rp2_common/hardware_clocks/include/hardware/clocks.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/clocks.h \
+ /home/brian/pico/pico-sdk/src/rp2_common/hardware_base/include/hardware/address_mapped.h \
+ /home/brian/pico/pico-sdk/src/rp2_common/hardware_watchdog/include/hardware/watchdog.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/watchdog.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/watchdog.h \
+ /home/brian/pico/pico-sdk/src/rp2_common/hardware_pll/include/hardware/pll.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/pll.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/pll.h \
+ /home/brian/pico/pico-sdk/src/rp2_common/hardware_xosc/include/hardware/xosc.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/xosc.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/xosc.h \
+ /home/brian/pico/pico-sdk/src/rp2_common/hardware_irq/include/hardware/irq.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/intctrl.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/m0plus.h \
+ /home/brian/pico/pico-sdk/src/rp2_common/hardware_gpio/include/hardware/gpio.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/sio.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/interp.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/padsbank0.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/pads_bank0.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/iobank0.h \
+ /home/brian/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/io_bank0.h
